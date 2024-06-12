134fc77f0b853a9b0ced5ea7925aa40b0589240f
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-12 19:57:12 +0300

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Type-annotated function add"""
+
+
+from typing import List
+def sum_list(input_list: List[float]) -> float:
+    """Takes two floats and returns their sum as float"""
+    s: float = 0.0
+    for i in input_list:
+        s += i
+    return s
