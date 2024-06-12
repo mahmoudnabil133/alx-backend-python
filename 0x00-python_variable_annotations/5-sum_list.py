1e1d0190c53286310e565991ebae69a7c5b380f3
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-12 19:58:47 +0300

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
+
 #!/usr/bin/env python3
-"""Type-annotated function add"""
-
-
+"""Type-annotated function sum_list"""
 from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
-    """Takes two floats and returns their sum as float"""
-    s: float = 0.0
+    """Takes a list of floats and returns their sum as float"""
+    a: float = 0.0
     for i in input_list:
-        s += i
-    return s
+        a += i
+    return a
